3e60d9c506c8fa6b1b4ef0a2b619a05904b6cb0a
Mariano Ferro <ferro.mariano@gmail.com>
2024-09-08 03:29:53 -0300

    init

---
+++ datain/__init__.py
@@ -0,0 +1,13 @@
+
+def main():
+    """
+    Función principal para iniciar la aplicación Flask y el WebSocket.
+    """
+    print("asd")
+    print("asd")
+    print("asd")
+    print("asd")
+    print("asd")
+
+if __name__ == '__main__':
+    main()
