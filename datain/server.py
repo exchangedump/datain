1c4c5c3c6b11e58ac573c91ab88fed5a66e5238a
Mariano Ferro <ferro.mariano@gmail.com>
2024-09-21 07:34:39 -0300

    añado soporte .env

--- datain/server.py
+++ datain/server.py
@@ -1,4 +1,5 @@
 import uvicorn
+import os
 from rich.console import Console
 
 from datain.stream.stream import Stream
@@ -19,31 +20,27 @@ console = Console()
 console.clear()
 
 if __name__ == "__main__":
-    
     _Stream = Stream()
-    # _Stream.start()
+    if 'STREAM_IN_AUTOSTART' in os.environ:
+        if os.environ['STREAM_IN_AUTOSTART'] == 'true':
+            _Stream.start()
     
     _wsControl = wsControl(_Stream)
-    
     
     # Crear una instancia de la clase ChatBotAPI
     _subscribeWebServer = subscribeWebServer(_Stream)
     _statusWebServer = statusWebServer(_Stream)
     _wsOutputWebServer = wsOutputWebServer(_wsControl, _Stream)
-    _htmlWebServer = htmlWebServer(_Stream)
     _controlWebServer = controlWebServer(_Stream)
 
     # Crear una instancia de FastAPI
     app = FastAPI()
-    app.mount('/static', StaticFiles(directory='datain/webServer/static', html=True), name='static')
-
 
     # Registrar las rutas definidas en la clase ChatBotAPI
     app.include_router(_subscribeWebServer.get_router())
     app.include_router(_statusWebServer.get_router())
     app.include_router(_wsOutputWebServer.get_router())
-    app.include_router(_htmlWebServer.get_router())
     app.include_router(_controlWebServer.get_router())
     
 
-    uvicorn.run(app, host="0.0.0.0", port=8880)
+    uvicorn.run(app, host=os.environ['SERVER_HOST'], port=os.environ['SERVER_PORT'])
